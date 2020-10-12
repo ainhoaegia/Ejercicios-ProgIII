d6eb667ef9e5c0ff52e6611748bd6402f8335ed6
ainhoaegia <ainhoa.egia@icloud.com>
2020-10-12 13:00:03 +0200

    Init Ejercicio 0.8

---
+++ Ejercicios-ProgIII/src/EJ08/tabla/iu/Threadable.java
@@ -0,0 +1,8 @@
+package EJ08.tabla.iu;
+
+/** Interfaz Runnable cuyo método run se ejecutará en un hilo independiente
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+public interface Threadable extends Runnable {
+	
+}
